cc7c38ace55f19a42eb1127798d2efd20154bff9
Simon Maher <simon.maher@integrify.io>
2019-06-03 12:58:17 +0300

    created random map function,props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react"
+import Ball from "./Ball"
+import Lottery from "./Lottery"
 class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Lottery />
+            </div>
+        )
+    }
 
 }
-
-
-
-
-
-
 
 export default App
